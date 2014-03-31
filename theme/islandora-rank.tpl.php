ab7f2cd02ec49b01251ff666741f037081f0bbdc
giancarlobi <giancarlo.birello@gmail.com>
2014-03-31 23:01:03 +0200

    List child of rank

--- theme/islandora-rank.tpl.php
+++ theme/islandora-rank.tpl.php
@@ -7,21 +7,25 @@
  * the islandora_porcus_preprocess_islandora_porcus
  * function.  Elements such as labels and buttons can be added here
  */
-$islandora_content = $islandora_object['BE']->content;
+
+
+//$islandora_content = $islandora_object['BE']->content;
 $path = drupal_get_path('module', 'islandora_solution_pack_biological_entity');
-drupal_add_css("$path/css/islandora_solution_pack_biological_entity.css");
-drupal_add_js("$path/js/islandora_solution_pack_biological_entity.js");
+//drupal_add_css("$path/css/islandora_solution_pack_biological_entity.css");
+//drupal_add_js("$path/js/islandora_solution_pack_biological_entity.js");
 ?>
 
 <div class="islandora-solution_pack_biological_entity-object islandora">
-  <button id="solution_pack_biological_entity_image">Bring Back</button>
+
+<?php print ("Is Taxonomic Level Of:");?>
+<?php foreach ($variables['related_taxa'] as $pid => $child) {
+
+?>
+
   <div class ="solution_pack_biological_entity_thumb">
-    <?php print $variables['islandora_thumbnail_img'] ?>
+    <?php print '<a href="/islandora/object/'.$pid.'">'.$child['title'].'</a>'; ?>
   </div>
-  <div class="islandora-basic-image-content-wrapper clearfix">
-    <?php if (isset($islandora_content)): ?>
-      <div class="islandora-solution_pack_biological_entity-content">
-        <?php print $islandora_content; ?>
-      </div>
-    <?php endif; ?>
-  </div>
+
+<?php
+  }
+?>
